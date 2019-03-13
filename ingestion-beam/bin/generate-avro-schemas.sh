8db0dcc629a44cfa8ad2d0c832a2626d16c49698
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-03-13 14:22:21 -0700

    Modify avro schema script to count and ignore failed conversions

--- ingestion-beam/bin/generate-avro-schemas.sh
+++ ingestion-beam/bin/generate-avro-schemas.sh
@@ -1,37 +1,69 @@
 #!/bin/bash
 # Generate a new archive with avro schemas from the mozilla-pipeline-schema repository
-set -eou pipefail
-set -x
+
+cd "$(dirname "$0")/.." || exit
 
 if [ ! -x  "$(command -v jsonschema_transpiler)" ]; then
     echo "jsonschema_transpiler is not installed"
-    cargo install --git https://github.com/acmiyaguchi/jsonschema-transpiler.git --branch dev
+    echo "Run 'cargo install --git https://github.com/acmiyaguchi/jsonschema-transpiler.git --branch dev'"
+    exit 1
 fi
 
+if [[ ! -f schemas.tar.gz ]]; then
+    echo "Run 'bin/download-schemas'"
+    exit 1
+fi
+
+# store the current directory
+finaldir=$(pwd)
+
+# create a temporary directory for work
+workdir=$(mktemp -d -t tmp.XXXXXXXXXX)
+function cleanup {
+  rm -rf "$workdir"
+  echo "Running cleanup!"
+}
+trap cleanup EXIT
+cd "$workdir" || exit
+
 # find all jsonschemas
-tar -xf schemas.tar.gz
-schemas=$(find mozilla-pipeline-schemas-gcp-ingestion-tests/schemas -type file -name "*.schema.json")
+inschema="mozilla-pipeline-schemas-gcp-ingestion-tests"
+tar -xf "$finaldir/schemas.tar.gz" -C "$workdir"
+schemas=$(find $inschema/schemas -type file -name "*.schema.json")
 
-# assume a new folder basename
-outfolder="avro-tests"
-mkdir $outfolder
+outschema="mps-avro-ingestion-tests"
 
+total=0
+failed=0
 # From a relative path to a jsonschema, transpile an avro schema in a mirrored directory.
-# Requires: $outfolder
+# Requires: $outschema, $total, $failed
 function generate_avro() {
     orig_path=$1
     # relative path to the root of the archive, using shell parameter expansion to strip the root
-    relpath=$(dirname $(echo ${orig_path#*/}))
-    avroname="$(basename $orig_path .schema.json).avro.json"
-    outpath="$outfolder/$relpath/$avroname"
+    relpath=$(dirname "${orig_path#*/}")
+    avroname="$(basename "$orig_path" .schema.json).avro.json"
+    outpath="$outschema/$relpath/$avroname"
     
     # create the folder to the new schema
-    mkdir -p "$outfolder/$relpath"
+    mkdir -p "$outschema/$relpath"
 
-    jsonschema_transpiler -f orig_path > $outpath
+    if ! jsonschema_transpiler --type avro -f "$orig_path" > "$outpath" 2> /dev/null; then
+        echo "Unable to convert $(basename "$orig_path")"
+        rm "$outpath"
+        ((failed++))
+    fi
+    ((total++))
 }
 
-for schema in $schemas 
-do
-    generate_avro $schema
+for schema in $schemas; do
+    generate_avro "$schema"
 done
+# see: https://github.com/acmiyaguchi/jsonschema-transpiler/pull/36
+echo "$failed/$total failed to convert"
+
+# prune folders from failed conversions
+find "$outschema" -type d -empty -delete
+
+# generate the final archive
+tar -zcf avro-schema.tar.gz "$outschema"
+cp avro-schema.tar.gz "$finaldir"
